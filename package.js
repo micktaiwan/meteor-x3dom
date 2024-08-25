f672b13581dc0987123ab32319cc1321f7dd203f
Mickael <mickael@lempire.co>
2024-08-25 15:10:30 +0200

    update for Meteor3

--- package.js
+++ package.js
@@ -1,11 +1,12 @@
 Package.describe({
   name: 'acemtp:x3dom',
   summary: 'Provides X3DOM library modified for use with Meteor.',
-  version: '1.5.1_2',
+  version: '1.5.1_3',
   git: 'https://github.com/acemtp/meteor-x3dom'
 });
 
 Package.onUse(function(api) {
   api.export('x3dom', 'client');
-  api.addFiles(['x3dom-full.debug.js', 'x3dom.css', 'x3dom.swf', 'meteor-x3dom.js'], 'client');
+  api.addFiles(['x3dom-full.debug.js', 'x3dom.css', 'meteor-x3dom.js'], 'client');
+  api.addAssets(['x3dom.swf'], 'client');
 });
